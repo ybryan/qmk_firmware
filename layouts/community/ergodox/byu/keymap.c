2acef57c9b6c9836f30ebe6ae73abf76c3078470
Bryan Yu <bryan.yu@santecapital.com>
2017-09-17 22:46:25 -0500

    Fix mouse and toggle layers

--- layouts/community/ergodox/byu/keymap.c
+++ layouts/community/ergodox/byu/keymap.c
@@ -5,7 +5,7 @@
 
 #define BASE 0 // default layer
 #define SYMB 1 // symbols
-#define MDIA 2 // media keys
+#define MOUS 2 // mouse keys
 #define MOVE 3 // movement layer
   
 #define TABPREV LSFT(LGUI(KC_LBRC))
@@ -23,8 +23,6 @@ enum custom_macros {
   VERSION,
   EEPROM,
 
-  TMUX_SUFFIX,
-  TMUX_CMD,
   OSX_PS_ALL,
   OSX_PS_APP,
 };
@@ -43,7 +41,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+------|  L1  |           |  L2  |------+------+------+------+------+--------|
  * | LShift |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |   /  | RShift |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   |LCTRL | LAFT | LGUI | LED  | MUTE |                                       | Left | Down |  Up  | Right|  L2  |
+ *   |LCTRL | LAFT | LGUI | LED  |L1MUTE|                                       | Left | Down |  Up  | Right|  L2  |
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,-------------.
  *                                        | VolD | VolU |       | TabP | TabN |
@@ -57,11 +55,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // Otherwise, it needs KC_*
 [BASE] = LAYOUT_ergodox(  // layer 0 : default
         // left hand
-        KC_GRV,        KC_1,          KC_2,   KC_3,   KC_4,          KC_5,   KC_MINS,
-        KC_TAB,        KC_Q,          KC_W,   KC_E,   KC_R,          KC_T,   ALL_T(KC_LBRC),
-        KC_ESC,        LT(MOVE, KC_A),KC_S,   KC_D,   KC_F,          KC_G,
-        KC_LSPO,       KC_Z,          KC_X,   KC_C,   KC_V,          KC_B,   TT(SYMB),
-        KC_LCTL,       KC_LALT,       KC_LGUI,BL_STEP,KC_MUTE,
+        KC_GRV,        KC_1,          KC_2,   KC_3,             KC_4,          KC_5,   KC_MINS,
+        KC_TAB,        KC_Q,          KC_W,   KC_E,             KC_R,          KC_T,   ALL_T(KC_LBRC),
+        KC_ESC,        LT(MOVE, KC_A),KC_S,   KC_D,             KC_F,          KC_G,
+        KC_LSPO,       KC_Z,          KC_X,   KC_C,             KC_V,          KC_B,   TT(SYMB),
+        KC_LCTL,       KC_LALT,       KC_LGUI,BL_STEP, LT(MOUS, KC_MUTE),
                                                                   KC_VOLD,      KC_VOLU,
                                                                                 KC_HOME,
                                                       KC_BSPC,    GUI_T(KC_DEL),KC_END,
@@ -69,8 +67,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_EQL,        KC_6,     KC_7,   KC_8,   KC_9,   KC_0,             KC_CAPS,
         MEH_T(KC_RBRC),KC_Y,     KC_U,   KC_I,   KC_O,   KC_P,             KC_BSLS,
                        KC_H,     KC_J,   KC_K,   KC_L,   LT(MOVE, KC_SCLN),KC_QUOT,
-        TT(MDIA),      KC_N,     KC_M,   KC_COMM,KC_DOT, KC_SLSH,          KC_RSPC,
-                                 KC_LEFT,KC_DOWN,KC_UP,  KC_RIGHT,         MO(SYMB),
+        TT(MOUS),      KC_N,     KC_M,   KC_COMM,KC_DOT, KC_SLSH,          KC_RSPC,
+                                 KC_LEFT,KC_DOWN,KC_UP,  KC_RIGHT,         MO(MOUS),
         TABPREV,       TABNEXT,
         KC_PGUP,
         KC_PGDN,       KC_ENT, KC_SPC
@@ -128,26 +126,26 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
  * |        |      |      |      |      |      |      |           |      |      |      |      |      |      |        |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   |      |      |      | Lclk | Rclk |                                       |      |      |      |      |      |
+ *   |      |      |      |      |      |                                       |      |      |      |      |      |
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,-------------.
  *                                        |      |      |       |      |      |
  *                                 ,------|------|------|       |------+------+------.
  *                                 |      |      |      |       |      |      |      |
- *                                 |      |      |------|       |------|      |      |
+ *                                 | Lclk | Rclk |------|       |------|      |      |
  *                                 |      |      |      |       |      |      |      |
  *                                 `--------------------'       `--------------------'
  */
 // MOUSE
-[MDIA] = LAYOUT_ergodox(
+[MOUS] = LAYOUT_ergodox(
        KC_TRNS, KC_TRNS, KC_TRNS,     KC_TRNS,    KC_TRNS, KC_TRNS, KC_TRNS,
        KC_TRNS, KC_TRNS, KC_TRNS,     KC_MS_U,    KC_TRNS, KC_TRNS, KC_TRNS,
        KC_TRNS, KC_TRNS, KC_MS_L,     KC_MS_D,    KC_MS_R, KC_TRNS,
        KC_TRNS, KC_TRNS, KC_TRNS,     KC_TRNS,    KC_TRNS, KC_TRNS, KC_TRNS,
-       KC_TRNS, KC_TRNS, KC_TRNS,     KC_BTN1,    KC_BTN2,
+       KC_TRNS, KC_TRNS, KC_TRNS,     KC_TRNS,    KC_TRNS,
                                                            KC_TRNS, KC_TRNS,
                                                                     KC_TRNS,
-                                                  KC_TRNS, KC_TRNS, KC_TRNS,
+                                                  KC_BTN1, KC_BTN2, KC_TRNS,
     // right hand
        KC_TRNS,  KC_TRNS, KC_TRNS,    KC_TRNS,    KC_TRNS, KC_TRNS, KC_TRNS,
        KC_TRNS,  KC_TRNS, KC_TRNS,    KC_TRNS,    KC_TRNS, KC_TRNS, KC_TRNS,
