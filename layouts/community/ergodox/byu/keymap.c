826c45e6952d8aa824f87761da64aec15bf09d39
Bryan Yu <bryan.yu@santecapital.com>
2017-09-18 13:20:01 -0500

    Move plus and minus to better spots

--- layouts/community/ergodox/byu/keymap.c
+++ layouts/community/ergodox/byu/keymap.c
@@ -33,11 +33,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* Keymap 0: Basic layer
  *
  * ,--------------------------------------------------.           ,--------------------------------------------------.
- * |   `    |   1  |   2  |   3  |   4  |   5  |  -   |           |   =  |   6  |   7  |   8  |   9  |   0  |  CAPS  |
+ * |   `    |   1  |   2  |   3  |   4  |   5  | PgUp |           | PgDn |   6  |   7  |   8  |   9  |   0  |  CAPS  |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
- * | TAB    |   Q  |   W  |   E  |   R  |   T  |  [   |           |   ]  |   Y  |   U  |   I  |   O  |   P  |   \    |
+ * | TAB    |   Q  |   W  |   E  |   R  |   T  |  [   |           |   ]  |LT3, Y|   U  |   I  |   O  |   P  |   \    |
  * |--------+------+------+------+------+------|Hyper |           | Meh  |------+------+------+------+------+--------|
- * | Esc    |LT 3,A|   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |LT 3,;|   '    |
+ * | Esc    |   A  |   S  |   D  |LT3, F|   G  |------|           |------|   H  |   J  |   K  |   L  |   ;  |   '    |
  * |--------+------+------+------+------+------|  L1  |           |  L2  |------+------+------+------+------+--------|
  * | LShift |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |   /  | RShift |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
@@ -46,32 +46,32 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                                        ,-------------.       ,-------------.
  *                                        | VolD | VolU |       | TabP | TabN |
  *                                 ,------|------|------|       |------+--------+------.
- *                                 |      |      | Home |       | PgUp |        |      |
+ *                                 |      |      |  F3  |       |  F4  |        |      |
  *                                 | Bcksp| Del/ |------|       |------|  Ent   |Space |
- *                                 |      | Gui  | End  |       | PgDn |        |      |
+ *                                 |      | Gui  |  -   |       |  =   |        |      |
  *                                 `--------------------'       `----------------------'
  */
 // If it accepts an argument (i.e, is a function), it doesn't need KC_.
 // Otherwise, it needs KC_*
 [BASE] = LAYOUT_ergodox(  // layer 0 : default
         // left hand
-        KC_GRV,        KC_1,          KC_2,   KC_3,             KC_4,          KC_5,   KC_MINS,
-        KC_TAB,        KC_Q,          KC_W,   KC_E,             KC_R,          KC_T,   ALL_T(KC_LBRC),
-        KC_ESC,        LT(MOVE, KC_A),KC_S,   KC_D,             KC_F,          KC_G,
-        KC_LSPO,       KC_Z,          KC_X,   KC_C,             KC_V,          KC_B,   TT(SYMB),
-        KC_LCTL,       KC_LALT,       KC_LGUI,BL_STEP, LT(SYMB, KC_MUTE),
-                                                                  KC_VOLD,      KC_VOLU,
-                                                                                KC_HOME,
-                                                      KC_BSPC,    GUI_T(KC_DEL),KC_END,
+        KC_GRV,        KC_1,   KC_2,   KC_3,   KC_4,             KC_5,         KC_PGDN,
+        KC_TAB,        KC_Q,   KC_W,   KC_E,   KC_R,             KC_T,         ALL_T(KC_LBRC),
+        KC_ESC,        KC_A,   KC_S,   KC_D,   LT(MOVE, KC_F),   KC_G,
+        KC_LSPO,       KC_Z,   KC_X,   KC_C,   KC_V,             KC_B,         TT(SYMB),
+        KC_LCTL,       KC_LALT,KC_LGUI,BL_STEP,LT(SYMB, KC_MUTE),
+                                                                 KC_VOLD,      KC_VOLU,
+                                                                               KC_F3,
+                                               KC_BSPC,          GUI_T(KC_DEL),KC_MINS,
         // right hand
-        KC_EQL,        KC_6,     KC_7,   KC_8,   KC_9,   KC_0,             KC_CAPS,
-        MEH_T(KC_RBRC),KC_Y,     KC_U,   KC_I,   KC_O,   KC_P,             KC_BSLS,
-                       KC_H,     KC_J,   KC_K,   KC_L,   LT(MOVE, KC_SCLN),KC_QUOT,
-        TT(MOUS),      KC_N,     KC_M,   KC_COMM,KC_DOT, KC_SLSH,          KC_RSPC,
-                                 KC_LEFT,KC_DOWN,KC_UP,  KC_RIGHT,         MO(MOUS),
+        KC_PGUP,       KC_6,          KC_7,   KC_8,   KC_9,  KC_0,KC_CAPS,
+        MEH_T(KC_RBRC),LT(MOVE, KC_Y),KC_U,   KC_I,   KC_O,  KC_P,KC_BSLS,
+                       KC_H,          KC_J,   KC_K,   KC_L,  KC_SCLN,     KC_QUOT,
+        TT(MOUS),      KC_N,          KC_M,   KC_COMM,KC_DOT,KC_SLSH,     KC_RSPC,
+                                      KC_LEFT,KC_DOWN,KC_UP, KC_RIGHT,    MO(MOUS),
         TABPREV,       TABNEXT,
-        KC_PGUP,
-        KC_PGDN,       KC_ENT, KC_SPC
+        KC_F4,
+        KC_EQL,        KC_ENT, KC_SPC
     ),
 /* Keymap 1: Symbol Layer
  *
@@ -89,9 +89,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                                        ,-------------.       ,-------------.
  *                                        |      |      |       |      |      |
  *                                 ,------|------|------|       |------+------+------.
- *                                 |      |      |APScr |       |      |      |      |
+ *                                 |      |      |APScr |       |PrScr |      |      |
  *                                 |      |      |------|       |------|      |      |
- *                                 |      |      | PScr |       |      |      |      |
+ *                                 |      |      |      |       |      |      |      |
  *                                 `--------------------'       `--------------------'
  */
 // SYMBOLS
@@ -104,7 +104,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
           EPRM,KC_AMPR,KC_ASTR,KC_TRNS,KC_TRNS,
                                        KC_TRNS,KC_TRNS,
                                                M(OSX_PS_APP),
-                               KC_TRNS,KC_TRNS,M(OSX_PS_ALL),
+                               KC_TRNS,KC_TRNS,KC_TRNS,
        // right hand
        KC_TRNS, KC_F6,   KC_F7,  KC_F8,  KC_F9,   KC_F10,  KC_F11,
        KC_TRNS, KC_TRNS,KC_7,   KC_8,    KC_9,    KC_BSLS, KC_F12,
@@ -112,7 +112,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
        KC_TRNS, KC_TRNS,KC_1,   KC_2,    KC_3,    KC_MINS, KC_TRNS,
                         KC_0,   KC_0,    KC_DOT,  KC_PLUS, KC_TRNS,
        KC_TRNS, KC_TRNS,
-       KC_TRNS,
+       M(OSX_PS_ALL),
        KC_TRNS, KC_TRNS, KC_TRNS
 ),
 /* Keymap 2: Mouse
