7e2a44e9a2d015c213eeb3db02381d2a363070d7
Bryan Yu <bryan.yu@santecapital.com>
2017-09-20 11:30:15 -0500

    Move version and eeprom to movement layer since we dont need it so often

--- layouts/community/ergodox/byu/keymap.c
+++ layouts/community/ergodox/byu/keymap.c
@@ -76,7 +76,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* Keymap 1: Symbol Layer
  *
  * ,---------------------------------------------------.           ,--------------------------------------------------.
- * |Version  |  F1  |  F2  |  F3  |  F4  |  F5  |      |           |      |  F6  |  F7  |  F8  |  F9  |  F10 |  F11   |
+ * |         |  F1  |  F2  |  F3  |  F4  |  F5  |PS_ALL|           |PS_APP|  F6  |  F7  |  F8  |  F9  |  F10 |  F11   |
  * |---------+------+------+------+------+------+------|           |------+------+------+------+------+------+--------|
  * |         |   !  |   @  |   (  |   )  |   |  |      |           |      |      |   7  |   8  |   9  |   \  |  F12   |
  * |---------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
@@ -84,29 +84,29 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |---------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
  * |         |   %  |   ^  |   [  |   ]  |   ~  |      |           |      |      |   1  |   2  |   3  |   -  |        |
  * `---------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   | EPRM  |   &  |   *  |      |      |                                       |   0  |   0  |   .  |   +  |      |
+ *   |       |   &  |   *  |      |      |                                       |   0  |   0  |   .  |   +  |      |
  *   `-----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,-------------.
  *                                        |      |      |       |      |      |
  *                                 ,------|------|------|       |------+------+------.
- *                                 |      |      |APScr |       |      |      |      |
+ *                                 |      |      |      |       |      |      |      |
  *                                 |      |      |------|       |------|      |      |
- *                                 |      |      |PrScr |       |      |      |      |
+ *                                 |      |      |      |       |      |      |      |
  *                                 `--------------------'       `--------------------'
  */
 // SYMBOLS
 [SYMB] = LAYOUT_ergodox(
        // left hand
-       VRSN,   KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_TRNS,
+       KC_TRNS,KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  M(OSX_PS_ALL),
        KC_TRNS,KC_EXLM,KC_AT,  KC_LPRN,KC_RPRN,KC_PIPE,KC_TRNS,
        KC_TRNS,KC_HASH,KC_DLR, KC_LCBR,KC_RCBR,KC_GRV,
        KC_TRNS,KC_PERC,KC_CIRC,KC_LBRC,KC_RBRC,KC_TILD,KC_TRNS,
-          EPRM,KC_AMPR,KC_ASTR,KC_TRNS,KC_TRNS,
+       KC_TRNS,KC_AMPR,KC_ASTR,KC_TRNS,KC_TRNS,
                                        KC_TRNS,KC_TRNS,
-                                               M(OSX_PS_APP),
-                               KC_TRNS,KC_TRNS,M(OSX_PS_ALL),
+                                               KC_TRNS,
+                               KC_TRNS,KC_TRNS,KC_TRNS,
        // right hand
-       KC_TRNS, KC_F6,   KC_F7,  KC_F8,  KC_F9,   KC_F10,  KC_F11,
+       M(OSX_PS_APP), KC_F6,   KC_F7,  KC_F8,  KC_F9,   KC_F10,  KC_F11,
        KC_TRNS, KC_TRNS,KC_7,   KC_8,    KC_9,    KC_BSLS, KC_F12,
                 KC_TRNS,KC_4,   KC_5,    KC_6,    KC_ASTR, KC_TRNS,
        KC_TRNS, KC_TRNS,KC_1,   KC_2,    KC_3,    KC_MINS, KC_TRNS,
@@ -159,7 +159,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* Keymap 3: Movement
  *
  * ,--------------------------------------------------.           ,--------------------------------------------------.
- * |  RESET |      |      |      |      |      |      |           |      |      |      |      |      |      | RESET  |
+ * |  RESET | VRSN |EEPROM|      |      |      |      |           |      |      |      |      |      |      | RESET  |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
  * |        |      |      |      |      |      |      |           |      |      |      |      |      |      |        |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
@@ -179,7 +179,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 // MOVEMENT
 [MOVE] = LAYOUT_ergodox(
-       RESET,   KC_TRNS,        KC_TRNS,        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+       RESET,   VRSN,           EPRM,        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
        KC_TRNS, KC_TRNS,        KC_TRNS,        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
        KC_TRNS, KC_TRNS,        PREVSC,         NEXTSC,  KC_TRNS, KC_TRNS,
        KC_TRNS, KC_TRNS,        KC_TRNS,        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
