971d947598a9cfffae20d141423c51c7c5163c08
Bryan Yu <bryan.yu@santecapital.com>
2017-09-20 13:13:05 -0500

    move quotation back since I'm already used to it being there and move brackets to easier to reach spot

--- layouts/community/ergodox/byu/keymap.c
+++ layouts/community/ergodox/byu/keymap.c
@@ -35,11 +35,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,--------------------------------------------------.           ,--------------------------------------------------.
  * |  -     |   1  |   2  |   3  |   4  |   5  |  F3  |           |  F4  |   6  |   7  |   8  |   9  |   0  |   =    |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
- * | TAB    |   Q  |   W  |   E  |   R  |   T  |  [   |           |   ]  |LT3, Y|   U  |   I  |   O  |   P  |   \    |
- * |--------+------+------+------+------+------|Hyper |           | Meh  |------+------+------+------+------+--------|
- * | G/Esc  |   A  |   S  |   D  |LT3, F|   G  |------|           |------|   H  |   J  |   K  |   L  |   ;  | Enter  |
- * |--------+------+------+------+------+------|  ~   |           |  '   |------+------+------+------+------+--------|
- * | LShift |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |   /  | RShift |
+ * | TAB    |   Q  |   W  |   E  |   R  |   T  |  `   |           |  L3  |   Y  |   U  |   I  |   O  |   P  |   \    |
+ * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
+ * | G/Esc  |   A  |   S  |   D  |LT3, F|   G  |------|           |------|   H  |   J  |   K  |   L  |   ;  |   '    |
+ * |--------+------+------+------+------+------|  [   |           |  ]   |------+------+------+------+------+--------|
+ * | LShift |   Z  |   X  |   C  |   V  |   B  |Hyper |           | Meh  |   N  |   M  |   ,  |   .  |   /  | RShift |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
  *   |LCTRL | LAFT |  LED | MUTE | LGUI |                                       | Left | Down |  Up  | Right|  L2  |
  *   `----------------------------------'                                       `----------------------------------'
@@ -56,18 +56,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [BASE] = LAYOUT_ergodox(  // layer 0 : default
         // left hand
         KC_MINS,       KC_1,   KC_2,   KC_3,   KC_4,             KC_5,            KC_F3,
-        KC_TAB,        KC_Q,   KC_W,   KC_E,   KC_R,             KC_T,            ALL_T(KC_LBRC),
+        KC_TAB,        KC_Q,   KC_W,   KC_E,   KC_R,             KC_T,            KC_GRV,
         KC_GESC,       KC_A,   KC_S,   KC_D,   LT(MOVE, KC_F),   KC_G,
-        KC_LSPO,       KC_Z,   KC_X,   KC_C,   KC_V,             KC_B,            KC_GRV,
+        KC_LSPO,       KC_Z,   KC_X,   KC_C,   KC_V,             KC_B,            ALL_T(KC_LBRC), 
         KC_LCTL,       KC_LALT,BL_TOGG,KC_MUTE,KC_LGUI,
                                                                  KC_VOLD,         KC_VOLU,
                                                                                   KC_CAPS,
                                                KC_BSPC,          LT(SYMB, KC_DEL),TT(SYMB),
         // right hand
         KC_F4,         KC_6,          KC_7,   KC_8,   KC_9,  KC_0,        KC_EQL,
-        MEH_T(KC_RBRC),LT(MOVE, KC_Y),KC_U,   KC_I,   KC_O,  KC_P,        KC_BSLS,
-                       KC_H,          KC_J,   KC_K,   KC_L,  KC_SCLN,     KC_ENT,
-        KC_QUOT,       KC_N,          KC_M,   KC_COMM,KC_DOT,KC_SLSH,     KC_RSPC,
+        TT(MOVE),      KC_Y,          KC_U,   KC_I,   KC_O,  KC_P,        KC_BSLS,
+                       KC_H,          KC_J,   KC_K,   KC_L,  KC_SCLN,     KC_QUOT,
+        MEH_T(KC_RBRC),KC_N,          KC_M,   KC_COMM,KC_DOT,KC_SLSH,     KC_RSPC,
                                       KC_LEFT,KC_DOWN,KC_UP, KC_RIGHT,    TT(MOUS),
         TABPREV,       TABNEXT,
         KC_PGUP,
