2b8a4f38b585df89ee4e8314d615d360067ecfec
Bryan Yu <bryan.yu@santecapital.com>
2017-10-10 13:46:01 -0500

    Update colors

--- layouts/community/ergodox/byu/visualizer.c
+++ layouts/community/ergodox/byu/visualizer.c
@@ -10,21 +10,28 @@ Note: this is a modified copy of ../default/visualizer.c, originally licensed GP
 // Also make sure that the buffer passed to state->layer_text remains valid until the previous animation is
 // stopped. This can be done by either double buffering it or by using constant strings
 static void get_visualizer_layer_and_color(visualizer_state_t* state) {
-    uint8_t saturation = 60;
+    // LCD_COLOR(  0,   0, 255); // White  <> RGB(255,255,255)                   
+    // LCD_COLOR( 85, 255, 255); // Green  <> RGB(  0,255,255)                   
+    // LCD_COLOR(  0, 255, 255); // Red    <> RGB(255,  0,255)                   
+    // LCD_COLOR(170, 255, 255); // Blue   <> RGB(  0,  0,255)                   
+    // LCD_COLOR(213, 255, 255); // Purple <> RGB(255,  0,255)                   
+    // LCD_COLOR( 42, 255, 255); // Yellow <> RGB(255,  0,255)                   
+    // LCD_COLOR(128, 255, 255); // Aqua   <> RGB(  0,255,255)    
+    uint8_t saturation = 0;
     if (state->status.leds & (1u << USB_LED_CAPS_LOCK)) {
-        saturation = 255;
+        saturation = 60;
     }
     if (state->status.layer & 0x8) {
-        state->target_lcd_color = LCD_COLOR(0, saturation, 0xFF);
+        state->target_lcd_color = LCD_COLOR(42, saturation, 255);
         state->layer_text = "3 - MOVEMENT";
     } else if (state->status.layer & 0x4) {
-        state->target_lcd_color = LCD_COLOR(168, saturation, 0xFF);
+        state->target_lcd_color = LCD_COLOR(170, 255, 255);
         state->layer_text = "2 - MOUSE";
     } else if (state->status.layer & 0x2) {
-        state->target_lcd_color = LCD_COLOR(216, 90, 0xFF);
+        state->target_lcd_color = LCD_COLOR(85, 255, 255);
         state->layer_text = "1 - SYMBOL";
     } else {
-        state->target_lcd_color = LCD_COLOR(84, saturation, 0xFF);
+        state->target_lcd_color = LCD_COLOR(0, 0, 255);
         state->layer_text = "DEFAULT";
     }
 }
