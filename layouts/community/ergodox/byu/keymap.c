845f0480cc1cb7a5b30df7095379e71384429598
Bryan Yu <bryan.yu@santecapital.com>
2017-10-13 01:11:38 -0500

    Datcyl version

--- layouts/community/ergodox/byu/keymap.c
+++ layouts/community/ergodox/byu/keymap.c
@@ -33,43 +33,43 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* Keymap 0: Basic layer
  *
  * ,--------------------------------------------------.           ,--------------------------------------------------.
- * |  =     |   1  |   2  |   3  |   4  |   5  |  F3  |           |  F4  |   6  |   7  |   8  |   9  |   0  |   -    |
+ * |  =     |   1  |   2  |   3  |   4  |   5  |      |           |      |   6  |   7  |   8  |   9  |   0  |   -    |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
- * | TAB    |   Q  |   W  |   E  |   R  |   T  |  `   |           |  L3  |   Y  |   U  |   I  |   O  |   P  |   \    |
+ * | TAB    |   Q  |   W  |   E  |   R  |   T  |      |           |      |   Y  |   U  |   I  |   O  |   P  | LT3, \ |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * | G/Esc  |   A  |   S  |   D  |LT3, F|   G  |------|           |------|   H  |   J  |   K  |   L  |   ;  |   '    |
- * |--------+------+------+------+------+------|  [   |           |  ]   |------+------+------+------+------+--------|
- * | LShift |   Z  |   X  |   C  |   V  |   B  |Hyper |           | Meh  |   N  |   M  |   ,  |   .  |   /  | RShift |
+ * |Hyp/GEsc|   A  |   S  |   D  |LT3, F|   G  |------|           |------|   H  |   J  |   K  |   L  |   ;  |   '    |
+ * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
+ * | LShift |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |   /  | RShift |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   |LCTRL | LAFT | TabP | TabN | LGUI |                                       | Left | Down |  Up  | Right|  L2  |
+ *   |LCtrl | LAlt | Left | Right| LGUI |                                       |   [  |   ]  | Down |  Up  |  L2  |
  *   `----------------------------------'                                       `----------------------------------'
- *                                        ,-------------.       ,-------------.
- *                                        |   [  |   ]  |       | Mute | LED  |
- *                                 ,------|------|------|       |------+--------+------.
- *                                 |      |      | CAPS |       | VolD |        |      |
- *                                 | Bcksp| Del  |------|       |------|  Ent   |Space |
- *                                 |      |      |  L1  |       | VolU |        |      |
- *                                 `--------------------'       `----------------------'
+ *                                      ,--------------.       ,-------------.
+ *                                      | PgDn  | PgUp |       | Mute | Meh  |
+ *                               ,------|-------|------|       |------+--------+------.
+ *                               |      |       | Caps |       | VolD |        |      |
+ *                               | Bcksp|  Del  |------|       |------|  Ent   |Space |
+ *                               |      |       |  L1  |       | VolU |        |      |
+ *                               `---------------------'       `----------------------'
  */
 // If it accepts an argument (i.e, is a function), it doesn't need KC_.
 // Otherwise, it needs KC_*
 [BASE] = LAYOUT_ergodox(  // layer 0 : default
         // left hand
-        KC_EQL,        KC_1,   KC_2,   KC_3,   KC_4,          KC_5,   KC_F3,
-        KC_TAB,        KC_Q,   KC_W,   KC_E,   KC_R,          KC_T,   KC_GRV,
-        KC_GESC,       KC_A,   KC_S,   KC_D,   LT(MOVE, KC_F),KC_G,
-        KC_LSPO,       KC_Z,   KC_X,   KC_C,   KC_V,          KC_B,   ALL_T(KC_LBRC), 
-        KC_LCTL,       KC_LALT,TABPREV,TABNEXT,KC_LGUI,
-                                                              KC_LBRC,KC_RBRC,
+        KC_EQL,        KC_1,   KC_2,   KC_3,    KC_4,          KC_5,   KC_TRNS,
+        KC_TAB,        KC_Q,   KC_W,   KC_E,    KC_R,          KC_T,   KC_TRNS,
+        ALL_T(KC_GESC),KC_A,   KC_S,   KC_D,    LT(MOVE, KC_F),KC_G,
+        KC_LSPO,       KC_Z,   KC_X,   KC_C,    KC_V,          KC_B,   KC_TRNS, 
+        KC_LCTL,       KC_LALT,KC_LEFT,KC_RIGHT,KC_LGUI,
+                                                              KC_PGDN,KC_PGUP,
                                                                       KC_CAPS,
                                                KC_BSPC,       KC_DEL, TT(SYMB),
         // right hand
-        KC_F4,         KC_6,   KC_7,   KC_8,   KC_9,  KC_0,        KC_MINS,
-        TT(MOVE),      KC_Y,   KC_U,   KC_I,   KC_O,  KC_P,        KC_BSLS,
-                       KC_H,   KC_J,   KC_K,   KC_L,  KC_SCLN,     KC_QUOT,
-        MEH_T(KC_RBRC),KC_N,   KC_M,   KC_COMM,KC_DOT,KC_SLSH,     KC_RSPC,
-                               KC_LEFT,KC_DOWN,KC_UP, KC_RIGHT,    TT(MOUS),
-        KC_MUTE,       BL_TOGG,
+        KC_TRNS,       KC_6,   KC_7,   KC_8,   KC_9,   KC_0,        KC_MINS,
+        KC_TRNS,       KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,        LT(MOVE, KC_BSLS),
+                       KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,     KC_QUOT,
+        KC_TRNS,       KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,     KC_RSPC,
+                               KC_LBRC,KC_RBRC,KC_DOWN,KC_UP,       TT(MOUS),
+        KC_MUTE,       KC_MEH,
         KC_VOLD,
         KC_VOLU,       KC_ENT, KC_SPC
     ),
