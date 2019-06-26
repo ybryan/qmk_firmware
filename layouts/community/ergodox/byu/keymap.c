d93fefb698ca02d3ca86e887dc5f388f708d2449
Bryan Yu <bryan.yu@santecapital.com>
2019-06-26 16:37:38 -0500

    Switch tab to more ergonomic spot and delete to tab

--- layouts/community/ergodox/byu/keymap.c
+++ layouts/community/ergodox/byu/keymap.c
@@ -35,7 +35,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,--------------------------------------------------.           ,--------------------------------------------------.
  * |  =     |   1  |   2  |   3  |   4  |   5  |      |           |      |   6  |   7  |   8  |   9  |   0  |   -    |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
- * | TAB    |   Q  |   W  |   E  |   R  |   T  |      |           |      |   Y  |   U  |   I  |   O  |   P  | LT3, \ |
+ * | Del    |   Q  |   W  |   E  |   R  |   T  |      |           |      |   Y  |   U  |   I  |   O  |   P  | LT3, \ |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
  * |Cmd/Esc |   A  |   S  |   D  |LT3, F|   G  |------|           |------|   H  |   J  |   K  |   L  |   ;  |   '    |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
@@ -47,7 +47,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                                      |  Cmd  |  `   |       | VolD | VolU |
  *                               ,------|-------|------|       |------+--------+------.
  *                               |      |       | Caps |       | Mute |        |      |
- *                               | Bcksp|  Del  |------|       |------|  Ent   |Space |
+ *                               | Bcksp|  Tab  |------|       |------|  Ent   |Space |
  *                               |      |       | Hyper|       | Meh  |        |      |
  *                               `---------------------'       `----------------------'
  */
@@ -56,13 +56,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [BASE] = LAYOUT_ergodox(  // layer 0 : default
         // left hand
         KC_EQL,        KC_1,         KC_2,   KC_3,         KC_4,          KC_5,   KC_TRNS,
-        KC_TAB,        KC_Q,         KC_W,   KC_E,         KC_R,          KC_T,   KC_TRNS,
+        KC_DEL,        KC_Q,         KC_W,   KC_E,         KC_R,          KC_T,   KC_TRNS,
         GUI_T(KC_ESC), KC_A,         KC_S,   KC_D,         LT(MOVE, KC_F),KC_G,
         KC_LSPO,       KC_Z,         KC_X,   KC_C,         KC_V,          KC_B,   KC_TRNS, 
         KC_LCTL,       ALT_T(KC_GRV),KC_LBRC,KC_RBRC,      TT(FUNC),
                                                               KC_LGUI,KC_GRV,
                                                                       KC_CAPS,
-                                               KC_BSPC,       KC_DEL, KC_HYPR,
+                                               KC_BSPC,       KC_TAB, KC_HYPR,
         // right hand
         KC_TRNS,       KC_6,   KC_7,         KC_8,         KC_9,       KC_0,        KC_MINS,
         KC_TRNS,       KC_Y,   KC_U,         KC_I,         KC_O,       KC_P,        LT(MOVE, KC_BSLS),
