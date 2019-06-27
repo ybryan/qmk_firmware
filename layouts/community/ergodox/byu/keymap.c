9fddb9f3a194e4ab1c9b5ef9824adcefeedf204a
Bryan Yu <bryan.yu@santecapital.com>
2019-06-27 13:38:27 -0500

    MAINT: Cleanup with visualizations

--- layouts/community/ergodox/byu/keymap.c
+++ layouts/community/ergodox/byu/keymap.c
@@ -48,20 +48,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // Otherwise, it needs KC_*
 [BASE] = LAYOUT_ergodox(  // layer 0 : default
         // left hand
-        KC_EQL,        KC_1,         KC_2,   KC_3,         KC_4,          KC_5,   KC_TRNS,
-        KC_DEL,        KC_Q,         KC_W,   KC_E,         KC_R,          KC_T,   KC_TRNS,
-        GUI_T(KC_ESC), KC_A,         KC_S,   KC_D,         LT(MOVE, KC_F),KC_G,
-        KC_LSPO,       KC_Z,         KC_X,   KC_C,         KC_V,          KC_B,   KC_TRNS, 
-        KC_LCTL,       ALT_T(KC_GRV),KC_LBRC,KC_RBRC,      TT(SYMB),
-                                                              KC_LGUI,KC_GRV,
-                                                                      KC_CAPS,
-                                               KC_BSPC,       KC_TAB, KC_HYPR,
+        KC_EQL,        KC_1,         KC_2,         KC_3,         KC_4,          KC_5,        KC_TRNS,
+        KC_DEL,        KC_Q,         KC_W,         KC_E,         KC_R,          KC_T,        KC_TRNS,
+        GUI_T(KC_ESC), KC_A,         KC_S,         KC_D,         LT(MOVE, KC_F),KC_G,
+        KC_LSPO,       KC_Z,         KC_X,         KC_C,         KC_V,          KC_B,        KC_TRNS, 
+        KC_LCTL,       ALT_T(KC_GRV),KC_LBRC,      KC_RBRC,      TT(SYMB),
+                                                                                   KC_LGUI, KC_GRV,
+                                                                                   KC_CAPS,
+                                                                                   KC_BSPC, KC_TAB, KC_HYPR,
         // right hand
-        KC_TRNS,       KC_6,   KC_7,         KC_8,         KC_9,       KC_0,        KC_MINS,
-        KC_TRNS,       KC_Y,   KC_U,         KC_I,         KC_O,       KC_P,        LT(MOVE, KC_BSLS),
-                       KC_H,   KC_J,         KC_K,         KC_L,       KC_SCLN,     KC_QUOT,
-        KC_TRNS,       KC_N,   KC_M,         KC_COMM,      KC_DOT,     KC_SLSH,     KC_RSPC,
-                               KC_LEFT,      KC_DOWN,      KC_UP,      KC_RIGHT,    TT(MOUS),
+        KC_TRNS,       KC_6,         KC_7,         KC_8,         KC_9,          KC_0,        KC_MINS,
+        KC_TRNS,       KC_Y,         KC_U,         KC_I,         KC_O,          KC_P,        LT(MOVE, KC_BSLS),
+                       KC_H,         KC_J,         KC_K,         KC_L,          KC_SCLN,     KC_QUOT,
+        KC_TRNS,       KC_N,         KC_M,         KC_COMM,      KC_DOT,        KC_SLSH,     KC_RSPC,
+                                     KC_LEFT,      KC_DOWN,      KC_UP,         KC_RIGHT,    TT(MOUS),
+
         KC_VOLD,       KC_VOLU,
         KC_MUTE,
         KC_MEH,        KC_ENT, KC_SPC
@@ -76,7 +77,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |---------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
  * |         |   1  |   2  |   3  |   4  |   5  |------|           |------|   6  |   7  |   8  |   9  |   0  |        |
  * |---------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * |         |  F11 | F12  |      |      |      |      |           |      |   `  |  [   |    ]  |      |      |        |
+ * |         |  F11 | F12  |      |      |      |      |           |      |   `  |  [   |   ]  |      |      |        |
  * `---------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
  *   |       |      |      |      |      |                                       |      |      |      |      |      |
  *   `-----------------------------------'                                       `----------------------------------'
@@ -107,6 +108,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_TRNS,
     KC_TRNS, KC_GRV,  KC_LBRC, KC_RBRC, KC_TRNS, KC_TRNS, KC_TRNS,
     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+
     KC_TRNS,      KC_TRNS,
     KC_TRNS,       
     KC_TRNS,      KC_TRNS, KC_TRNS
